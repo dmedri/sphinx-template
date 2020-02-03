37137674095f22882de099e043c8c1cef92f36d3
Daniele Medri <dmedri@gmail.com>
2020-02-03 13:45:59 +0100

    Update po2mo.sh

--- po2mo.sh
+++ po2mo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-for file in `find . -name "*.po"` ; do msgfmt -vv -o ${file/.po/.mo} $file ; done
+for file in `find . -name "*.po"`;
+  do
+    msgfmt -vv -o ${file/.po/.mo} $file;
+  done
